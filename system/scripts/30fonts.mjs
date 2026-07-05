4851f777791258dd0c6be419714baaf88497b0f6
SollyBunny <sollybunn@gmail.com>
2026-07-06 00:21:47 +0100

    Add more okgs

---
+++ system/scripts/30fonts.mjs
@@ -0,0 +1,5 @@
+import { pacmanInstall } from "#shared/shared.mjs";
+
+await pacmanInstall(
+    "noto-fonts", "noto-fonts-cjk", "noto-fonts-emoji"
+);
--- system/scripts/30utils.mjs
+++ system/scripts/30utils.mjs
@@ -1,8 +1,8 @@
 import { pacmanInstall } from "#shared/shared.mjs";
 
 await pacmanInstall(
-    "mpv", "htop",
-    "curl", "wget",
+    "mpv", "htop", "fastfetch",
+    "curl", "wget", "ssh",
     "nodejs", "npm",
     "zip", "unrar", "tar",
     "base-devel", "clang", "llvm", "just", "jemalloc",
