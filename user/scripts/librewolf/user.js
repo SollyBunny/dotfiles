392ba0c7bb3c9a72f9b78fde7ba6718018c0f280
SollyBunny <sollybunn@gmail.com>
2026-07-05 18:21:18 +0100

    more tor fixes

--- user/scripts/librewolf/user.js
+++ user/scripts/librewolf/user.js
@@ -7,5 +7,7 @@ user_pref("sidebar.visibility", "hide-sidebar");
 user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[\"preferences-button\",\"logins-button\",\"developer-button\"],\"unified-extensions-area\":[\"_74186d10-f6f2-4f73-b33a-83bb72e50654_-browser-action\"],\"nav-bar\":[\"sidebar-button\",\"back-button\",\"forward-button\",\"stop-reload-button\",\"vertical-spacer\",\"urlbar-container\",\"zoom-controls\",\"downloads-button\",\"fxa-toolbar-menu-button\",\"reset-pbm-toolbar-button\",\"unified-extensions-button\",\"ublock0_raymondhill_net-browser-action\",\"addon_darkreader_org-browser-action\",\"pywalfox_frewacom_org-browser-action\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"vertical-tabs\":[],\"PersonalToolbar\":[\"personal-bookmarks\"]},\"seen\":[\"reset-pbm-toolbar-button\",\"developer-button\",\"screenshot-button\",\"ublock0_raymondhill_net-browser-action\",\"pywalfox_frewacom_org-browser-action\",\"_74186d10-f6f2-4f73-b33a-83bb72e50654_-browser-action\",\"addon_darkreader_org-browser-action\"],\"dirtyAreaCache\":[\"nav-bar\",\"vertical-tabs\",\"unified-extensions-area\",\"TabsToolbar\",\"toolbar-menubar\",\"PersonalToolbar\",\"widget-overflow-fixed-list\"],\"currentVersion\":24,\"newElementCount\":3}");
 
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.tabs.allow_transparent_browser");
+user_pref("widget.gtk.native-context-menus");
 
 user_pref("browser.aboutConfig.showWarning", false);
