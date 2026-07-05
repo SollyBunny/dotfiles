e430480754592103dbc3d1e409a08fa86b1b4cbd
SollyBunny <sollybunn@gmail.com>
2026-07-05 23:45:09 +0100

    add awaits in 50browser.mjs

--- system/scripts/50browser.mjs
+++ system/scripts/50browser.mjs
@@ -1,4 +1,4 @@
 import { commandExists, yayInstall } from "#shared/shared.mjs";
 
-if (!commandExists("librewolf"))
-    yayInstall("librewolf-bin");
+if (!await commandExists("librewolf"))
+    await yayInstall("librewolf-bin");
