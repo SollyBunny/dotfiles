840b898c427ef2629c7e22e475f2352d688312b1
SollyBunny <sollybunn@gmail.com>
2026-07-07 00:14:50 +0100

    Add noctalia deps

--- system/scripts/50gui.mjs
+++ system/scripts/50gui.mjs
@@ -8,3 +8,8 @@ await runShellRoot("systemctl enable greetd");
 await pacmanInstall("kitty", "pavucontrol", "zed");
 
 await yayInstall("mangowm", "noctalia-git", "noctalia-greeter-git");
+
+// Required by noctalia GTK template
+await pacmanInstall("adw-gtk-theme", "nwg-look");
+
+await yayInstall("python-pywalfox");
