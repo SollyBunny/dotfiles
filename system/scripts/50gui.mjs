02b8a7b79e596f9f5ca81ded3305e27a8845cdf7
SollyBunny <sollybunn@gmail.com>
2026-07-06 23:16:28 +0000

    Add ddcutil for monitor control

--- system/scripts/50gui.mjs
+++ system/scripts/50gui.mjs
@@ -1,6 +1,6 @@
 import { pacmanInstall, runShellRoot, yayInstall } from "#shared/shell.mjs";
 
-await pacmanInstall("xorg-xwayland", "cage");
+await pacmanInstall("xorg-xwayland", "cage", "ddcutil");
 
 await pacmanInstall("greetd");
 await runShellRoot("systemctl enable greetd");
@@ -9,7 +9,6 @@ await pacmanInstall("kitty", "pavucontrol", "zed");
 
 await yayInstall("mangowm", "noctalia-git", "noctalia-greeter-git");
 
-// Required by noctalia GTK template
+// Required by noctalia templates
 await pacmanInstall("adw-gtk-theme", "nwg-look");
-
 await yayInstall("python-pywalfox");
