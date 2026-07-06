440ee705a08117d1cdf705c634150f598057eeb1
SollyBunny <sollybunn@gmail.com>
2026-07-06 23:11:49 +0000

    Add pkg lynx, nano, patch

--- system/scripts/30utils.mjs
+++ system/scripts/30utils.mjs
@@ -1,12 +1,12 @@
 import { pacmanInstall } from "#shared/shell.mjs";
 
 await pacmanInstall(
-	"mpv", "htop", "fastfetch", "micro", "numbat", "cpupower",
-	"curl", "wget", "openssh",
+	"lynx", "mpv", "micro", "nano", "fastfetch", "numbat",
+	"htop", "cpupower",
+	"curl", "wget", "openssh", "git-credential-oauth",
 	"nodejs", "npm",
 	"zip", "unzip", "unrar", "tar",
 	"base-devel", "clang", "llvm", "jemalloc", "pkgconf",
 	"just", "meson", "ninja",
-	"less", "which", "tree", "jq",
-	"git-credential-oauth",
+	"less", "which", "tree", "jq", "patch",
 );
