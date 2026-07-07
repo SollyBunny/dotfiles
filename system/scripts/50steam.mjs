119383578d139b061185278b2d4157a42015dac2
SollyBunny <sollybunn@gmail.com>
2026-07-07 01:42:18 +0000

    Add lil hint about steam setup

--- system/scripts/50steam.mjs
+++ system/scripts/50steam.mjs
@@ -1,4 +1,7 @@
 import { pacmanInstall, yayInstall } from "#shared/shell.mjs";
 
+// Might need this
+// https://steamcommunity.com/discussions/forum/1/617328415077879473/
+
 await pacmanInstall("steam");
 await yayInstall("millennium-bin");
