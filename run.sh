ec82062dde9b18c9c1f09e602811e7300b414307
SollyBunny <sollybunn@gmail.com>
2026-07-08 14:50:22 +0000

    Improve run.sh script

--- run.sh
+++ run.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 
+help() {
+	echo "Usage: $0 -h [$(printf "%s\n" */run.mjs | xargs -n1 dirname | paste -sd "|")]"
+	exit 1
+}
+
+if [[ $# -ne 0 && $# -ne 1 ]]; then
+	help
+fi
+
 if ! command -v node >/dev/null 2>&1; then
 	su -c "pacman -Sy --noconfirm nodejs"
 fi
 
-if [ "$#" -gt 0 ]; then
-	node "$@"
-else
+if [[ $# -eq 0 ]]; then
 	node ./run.mjs
+else
+	if [[ -f "$1/run.mjs" ]]; then
+		node "$1/run.mjs"
+	else
+		echo "Error: $1/run.mjs does not exist"
+		help
+	fi
 fi
-
