ed7994317e76240eb0c832a2832e587fa6ab6a3b
SollyBunny <sollybunn@gmail.com>
2026-07-09 18:23:48 +0000

    Add wayland variables

--- user/config/.bashrc.d/vars.sh
+++ user/config/.bashrc.d/vars.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 export EDITOR=micro
+
+# Wayland
+export GDK_BACKEND=wayland
+export QT_QPA_PLATFORM=wayland
+export SDL_VIDEODRIVER=wayland
+export MOZ_ENABLE_WAYLAND=1
+export _JAVA_AWT_WM_NONREPARENTING=1
