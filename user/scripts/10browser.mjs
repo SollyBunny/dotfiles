3246669379fffe39565d6f1da90f0fc240e50f25
SollyBunny <sollybunn@gmail.com>
2026-07-12 14:09:50 +0100

    Never nest in 10browser.mjs

--- user/scripts/10browser.mjs
+++ user/scripts/10browser.mjs
@@ -13,14 +13,14 @@ const profileRoot = path.join(os.homedir(), ".config/librewolf/librewolf");
 for (let profile of await fs.readdir(profileRoot)) {
 	profile = path.join(profileRoot, profile);
 	const stat = await fs.stat(path.join(profile, "prefs.js"), { throwIfNoEntry: false });
-	if (stat) {
-		console.log(`Installing for profile ${profile}`);
-		for (const file of await fs.readdir(dataRoot)) {
-			const target = path.join(dataRoot, file);
-			const linkPath = path.join(profile, file);
-			const targetRel = path.relative(path.dirname(linkPath), target);
-			await moveToBackup(linkPath);
-			await fs.symlink(targetRel, linkPath);
-		}
+	if (!stat)
+		continue;
+	console.log(`Installing for profile ${profile}`);
+	for (const file of await fs.readdir(dataRoot)) {
+		const target = path.join(dataRoot, file);
+		const linkPath = path.join(profile, file);
+		const targetRel = path.relative(path.dirname(linkPath), target);
+		await moveToBackup(linkPath);
+		await fs.symlink(targetRel, linkPath);
 	}
 }
