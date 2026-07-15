12ae225e0efdce8d4c9381d4427e3b7d4555b8de
SollyBunny <sollybunn@gmail.com>
2026-07-15 14:23:23 +0100

    Add missing awaits to 10git.mjs

--- user/scripts/10git.mjs
+++ user/scripts/10git.mjs
@@ -2,6 +2,6 @@ import { runShell } from "#shared/shell.mjs";
 
 const timeoutHours = 6;
 
-runShell("git config --global --unset-all credential.helper");
-runShell(`git config --global --add credential.helper "cache --timeout ${timeoutHours * 60 * 60}"`);
-runShell("git config --global --add credential.helper oauth");
+await runShell("git config --global --unset-all credential.helper");
+await runShell(`git config --global --add credential.helper "cache --timeout ${timeoutHours * 60 * 60}"`);
+await runShell("git config --global --add credential.helper oauth");
