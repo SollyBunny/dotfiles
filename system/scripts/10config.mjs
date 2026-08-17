cec925e94f9efeefdac188d8a0180c4dbe943869
SollyBunny <76408142+SollyBunny@users.noreply.github.com>
2026-08-17 19:54:23 +0100

    Restrict mode to 440 for sudoers-rs

--- system/scripts/10config.mjs
+++ system/scripts/10config.mjs
@@ -5,6 +5,9 @@ import path from "node:path";
 
 // Run after modifying ../conf/
 
+// These files install with 440 instead of 644
+const RESTRICTED_FILES = ["sudoers-rs"];
+
 const __dir = getThisDir(import.meta.url);
 
 const configRoot = path.join(__dir, "../config");
@@ -17,15 +20,16 @@ const files = (await fs.readdir(configRoot, {
 	.map(v => path.join(v.parentPath, v.name));
 
 for (const file of files) {
+	const mode = RESTRICTED_FILES.indexOf(path.parse(file).base) === -1 ? 0o644 : 0o440;
 	const fileInstall = path.join(path.sep, path.relative(configRoot, file));
 	const fileInstallStat = await lstatSafe(fileInstall);
 	if (fileInstallStat && fileInstallStat.isFile() && await filesEqual(file, fileInstall)) {
 		if (fileInstallStat.gid !== 0 || fileInstallStat.uid !== 0)
 			await runShellRoot(`chown -- root:root "$fileInstall"`, { fileInstall });
-		if ((fileInstallStat.mode & 0o777) !== 0o644)
-			await runShellRoot(`chmod -- 644 "$fileInstall"`, { fileInstall });
+		if ((fileInstallStat.mode & 0o777) !== mode)
+			await runShellRoot(`chmod -- ${mode.toString(8)} "$fileInstall"`, { fileInstall });
 		continue;
 	}
 	await moveToBackup(fileInstall);
-	await runShellRoot(`install -D -p -v --mode=644 --group=root --owner=root -- "$file" "$fileInstall"`, { file, fileInstall })
+	await runShellRoot(`install -D -p -v --mode=${mode.toString(8)} --group=root --owner=root -- "$file" "$fileInstall"`, { file, fileInstall })
 }
