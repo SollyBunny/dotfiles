19f7ac846ecc7848d98c1f041de3dfaf56f90386
SollyBunny <76408142+SollyBunny@users.noreply.github.com>
2026-09-16 18:29:52 +0100

    apps: Add keepassxc

--- system/scripts/50apps.mjs
+++ system/scripts/50apps.mjs
@@ -3,5 +3,6 @@ import { pacmanInstall, yayInstall } from "#shared/install.mjs";
 await pacmanInstall("kitty", "pavucontrol", "zed", "qbittorrent", "gimp");
 await pacmanInstall("mpv", "mpv-mpris");
 await pacmanInstall("libreoffice-fresh");
+await pacmanInstall("keepassxc");
 
 await yayInstall("rustdesk-bin");
